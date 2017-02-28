2f37d286daaea9bde9b1c862f2e40b5c123c5b3f
Jonathan <jonathan.gunnells@yahoo.com>
2017-02-27 22:34:46 -0400

    Removed combat simulator, added method to roll stats and intro to story dialogue

--- src/Main.java
+++ src/Main.java
@@ -4,25 +4,35 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        while (true) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("You run into a Troll, do you wish to fight? [Y/N}]");
-            String answer = scanner.nextLine();
-            if (answer.equalsIgnoreCase("y")) {
-                int roll = Player.roll20();
-                if (roll < 10) {
-                    System.out.println("you lose");
-                } else {
-                    System.out.println("The Troll is Cleft in Twain!");
-                }
+        Player.rollStat();
 
-
-            }
-
-
-        }
-
+//        Scanner scan = new Scanner(System.in);
+//        System.out.println("Hello traveler, what is your name?");
+//        String name = scan.nextLine();
+//        System.out.println("Welcome " + name + ", let us begin your adventure");
+//        System.out.println("First we must create your character");
+//
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("You run into a Troll, do you wish to fight? [Y/N}]");
+//        String answer = scanner.nextLine();
+//        if (answer.equalsIgnoreCase("y")) {
+//            int roll = Player.roll20();
+//            if (roll < 10) {
+//                System.out.println("you lose");
+//            } else {
+//                System.out.println("The Troll is Cleft in Twain!");
+//
+//            }
+//
+//
+//        }
+//
+//
     }
+
+
+
+
 }
 
 
--- src/Player.java
+++ src/Player.java
@@ -1,25 +1,31 @@
 import java.util.Random;
 public class Player {
+    int health;
+
 
     public static void roll4(){
+        System.out.println("rolling D4");
         Random rando = new Random();
         int number = rando.nextInt(4);
         System.out.println(number);
     }
 
     public static void roll6(){
+        System.out.println("rolling D6");
         Random rand = new Random();
         int number = rand.nextInt(6);
         System.out.println(number);
     }
 
     public static void roll8(){
+        System.out.println("rolling D8");
         Random rand = new Random();
         int number = rand.nextInt(8);
         System.out.println(number);
     }
 
     public static void roll10(){
+        System.out.println("rolling D10");
         Random rand = new Random();
         int number = rand.nextInt(10);
         System.out.println(number);
@@ -27,6 +33,7 @@ public class Player {
     }
 
     public static int roll12(){
+        System.out.println("rolling D12");
         Random rand = new Random();
         int number = rand.nextInt(12);
         System.out.println(number);
@@ -34,12 +41,32 @@ public class Player {
     }
 
     public static int roll20(){
+        System.out.println("rolling D20");
         Random rand = new Random();
         int number = rand.nextInt(20);
+        if (number == 1){
+            System.out.println("Critical Failure!");
+        }
         System.out.println(number);
+
         return number;
     }
 
+    public static int rollStat(){
+        Random rand = new Random();
+        int first = rand.nextInt(6);
+        int second = rand.nextInt(6);
+        int third = rand.nextInt(6);
+        int stat = first + second + third;
+        System.out.println("Rolling 3 D6");
+        System.out.println(first + " + " + second + " + " + third + " = " + stat);
+        return stat;
 
 
+    }
 }
+
+
+
+
+
